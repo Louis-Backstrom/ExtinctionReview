b32dbc7afb0adc57064edcc84be87392dcf64f3c
Louis Backstrom <44354406+Louis-Backstrom@users.noreply.github.com>
2026-03-12 14:29:35 +0000

    better comparison between scenario A simulations
    few tweaks to the simulations themselves

--- Render All.R
+++ Render All.R
@@ -2,5 +2,6 @@ library(quarto)
 
 quarto_render("A - Extinct.qmd")
 quarto_render("A - Extant.qmd")
+quarto_render("A - Comparison.qmd")
 quarto_render("B - Extinct.qmd")
 quarto_render("B - Extant.qmd")
