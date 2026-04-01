1c5c6ef883f8df3cbbb312704b34af63a88ef596
Louis Backstrom <44354406+Louis-Backstrom@users.noreply.github.com>
2026-04-01 15:32:33 +0100

    first attempt at scenario C

--- Render All.R
+++ Render All.R
@@ -3,5 +3,16 @@ library(quarto)
 quarto_render("A - Extinct.qmd")
 quarto_render("A - Extant.qmd")
 quarto_render("A - Comparison.qmd")
+
 quarto_render("B - Extinct.qmd")
 quarto_render("B - Extant.qmd")
+
+quarto_render("C1 - Extinct.qmd")
+quarto_render("C2 - Extinct.qmd")
+quarto_render("C3 - Extinct.qmd")
+quarto_render("C4 - Extinct.qmd")
+
+quarto_render("C1 - Extant.qmd")
+quarto_render("C2 - Extant.qmd")
+quarto_render("C3 - Extant.qmd")
+quarto_render("C4 - Extant.qmd")
